089735f0c6955003dca3ce59b44a7fca9a214422
Taiwo <taiwotunde32@gmail.com>
2023-07-01 21:54:02 +0100

    Election Monitoring Website

--- client-side/src/component/IndividualPU.js
+++ client-side/src/component/IndividualPU.js
@@ -16,146 +16,55 @@ export const IndividualPU = () => {
     ({ entered_by_user, date_entered, user_ip_address, ...rest }) => rest
   );
 
-  const itemsWithUniqueId8 = modifiedVotes.filter(
-    (item) => item.polling_unit_uniqueid === "8"
-  );
+  // Create a map of polling_unit_uniqueid to their corresponding data
+  const pollingUnitDataMap = modifiedVotes.reduce((acc, data) => {
+    const { polling_unit_uniqueid } = data;
+    if (!acc[polling_unit_uniqueid]) {
+      acc[polling_unit_uniqueid] = [];
+    }
+    acc[polling_unit_uniqueid].push(data);
+    return acc;
+  }, {});
 
-  const tableHeading = itemsWithUniqueId8[0];
+  // Get the unique polling_unit_uniqueid values
+  const uniquePollingUnitIds = Object.keys(pollingUnitDataMap);
 
-  // Get the keys of the first object
-  const headings = Object.keys(tableHeading);
+  // Create table components dynamically for each polling_unit_uniqueid
+  const tables = uniquePollingUnitIds.map((unitId) => {
+    const tableData = pollingUnitDataMap[unitId];
+    const tableHeading = tableData[0];
+    const headings = Object.keys(tableHeading);
 
-  // get table heading data
-  const ThData = () => {
-    return headings.map((data) => {
-      return <th key={data.result_id}>{data}</th>;
-    });
-  };
+    const ThData = () => {
+      return headings.map((data) => {
+        return <th key={data.result_id}>{data}</th>;
+      });
+    };
 
-  // get table row data
-  const tdData = () => {
-    return itemsWithUniqueId8.map((data) => {
-      return (
-        <tr>
-          {headings.map((v) => {
-            return <td>{data[v]}</td>;
-          })}
-        </tr>
-      );
-    });
-  };
+    const tdData = () => {
+      return tableData.map((data) => {
+        return (
+          <tr key={data.result_id}>
+            {headings.map((v) => {
+              return <td key={v}>{data[v]}</td>;
+            })}
+          </tr>
+        );
+      });
+    };
 
-  // FOR POLING UNIT 9
+    return (
+      <div key={unitId}>
+        <h3> RESULTS FOR POLLING UNIT {unitId} </h3>
+        <table className="table">
+          <thead>
+            <tr>{ThData()}</tr>
+          </thead>
+          <tbody>{tdData()}</tbody>
+        </table>
+      </div>
+    );
+  });
 
-  const itemsWithUniqueId9 = modifiedVotes.filter(
-    (item) => item.polling_unit_uniqueid === "9"
-  );
-
-  // get table heading data
-  const ThDataUnit9 = () => {
-    return headings.map((data) => {
-      return <th key={data.result_id}>{data}</th>;
-    });
-  };
-
-  // get table row data
-  const tdDataUnit9 = () => {
-    return itemsWithUniqueId9.map((data) => {
-      return (
-        <tr>
-          {headings.map((v) => {
-            return <td>{data[v]}</td>;
-          })}
-        </tr>
-      );
-    });
-  };
-
-  // FOR POLING UNIT 10
-
-  const itemsWithUniqueId10 = modifiedVotes.filter(
-    (item) => item.polling_unit_uniqueid === "10"
-  );
-
-  // get table heading data
-  const ThDataUnit10 = () => {
-    return headings.map((data) => {
-      return <th key={data.result_id}>{data}</th>;
-    });
-  };
-
-  // get table row data
-  const tdDataUnit10 = () => {
-    return itemsWithUniqueId10.map((data) => {
-      return (
-        <tr>
-          {headings.map((v) => {
-            return <td>{data[v]}</td>;
-          })}
-        </tr>
-      );
-    });
-  };
-
-  // FOR POLING UNIT 11
-
-  const itemsWithUniqueId11 = modifiedVotes.filter(
-    (item) => item.polling_unit_uniqueid === "11"
-  );
-
-  // get table heading data
-  const ThDataUnit11 = () => {
-    return headings.map((data) => {
-      return <th key={data.result_id}>{data}</th>;
-    });
-  };
-
-  // get table row data
-  const tdDataUnit11 = () => {
-    return itemsWithUniqueId11.map((data) => {
-      return (
-        <tr>
-          {headings.map((v) => {
-            return <td>{data[v]}</td>;
-          })}
-        </tr>
-      );
-    });
-  };
-
-  return (
-    <div>
-      <h3> RESULTS FOR POLING UNIT 8 </h3>
-      <table className="table">
-        <thead>
-          <tr>{ThData()}</tr>
-        </thead>
-        <tbody>{tdData()}</tbody>
-      </table>
-
-      <h3> RESULTS FOR POLING UNIT 9 </h3>
-      <table className="table">
-        <thead>
-          <tr>{ThDataUnit9()}</tr>
-        </thead>
-        <tbody>{tdDataUnit9()}</tbody>
-      </table>
-
-      <h3> RESULTS FOR POLING UNIT 10 </h3>
-      <table className="table">
-        <thead>
-          <tr>{ThDataUnit10()}</tr>
-        </thead>
-        <tbody>{tdDataUnit10()}</tbody>
-      </table>
-
-      <h3> RESULTS FOR POLING UNIT 11 </h3>
-      <table className="table">
-        <thead>
-          <tr>{ThDataUnit11()}</tr>
-        </thead>
-        <tbody>{tdDataUnit11()}</tbody>
-      </table>
-    </div>
-  );
+  return <div>{tables}</div>;
 };
